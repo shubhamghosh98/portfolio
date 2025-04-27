db508ba863a36cded0707fd10f83dc5a05618ff1
Shubham Ghosh <bughunter216@gmail.com>
2025-04-27 18:54:59 +0530

    adding changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,21 +6,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* Use basename to ensure proper routing when deployed on GitHub Pages */}
+        <Router base="/portfolio">
+          <Route path="/" component={Home} />
+          <Route component={NotFound} />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
